0c6b35c35d9c5b266c88832bb08a5eef4c6bb0c2
Nemeczek <nemeczekes@gmail.com>
2015-01-05 17:03:09 +0100

    Added replace for main.rb

---
+++ lib/CalculateWrapper.rb
@@ -0,0 +1,24 @@
+require 'wektor.rb'
+
+
+def Calculate(x,y) do
+
+end
+
+#Wywalić input gdzieś indziej
+#tutaj tylko chcemy mieć metode calculate do której wrzucamy co chcemy otrzymać z aplikacji
+#puts "Podaj ilość węzłów:"
+#n = gets.chomp
+
+#X = Array.new(n)
+#Y = Array.new(n)
+#P = Hash.new {|h,k| h[k] = Wektor.new(n)}
+
+#for i in (0...n) do
+#	puts "Podaj x[#{i}]:"
+#	X[i] = gets.chomp.to_i
+#
+#	puts "Podaj y[#{i}]:"
+#	Y[i] = gets.chomp.to_i
+#end
+
