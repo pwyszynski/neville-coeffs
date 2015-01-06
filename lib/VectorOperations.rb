3757e528a1df6d8c25b7435415c086a368820cbf
padamaszek <Ursus707@gmail.com>
2015-01-06 19:39:27 +0100

    v2 more like ruby and new nawiases

--- lib/VectorOperations.rb
+++ lib/VectorOperations.rb
@@ -4,27 +4,27 @@ class WektorOperations
 
   def Coefficients(i, j, tabX, tabY, pyramid)
     #mnozymy przez Xj
-    temp = pyramid[i,j-1]
+    temp = pyramid[i][j-1]
     temp.MultiplyBy(tabX[j])
-    pyramid[i,j].SetVector(temp)
+    pyramid[i][j].SetVector(temp)
 
     #odjęcie Pi,j-1 przesuniętego o 1 w prawo
-    temp = pyramid[i,j-1]
+    temp = pyramid[i][j-1]
     temp.ShiftRight
-    pyramid[i,j].Sub(temp)
+    pyramid[i][j].Sub(temp)
 
     #dodanie Pi+1, j przesuniętego o 1 w prawo
-    temp = pyramid[i+1,j]
+    temp = pyramid[i+1][j]
     temp.ShiftRight
-    pyramid[i,j].Add(temp)
+    pyramid[i][j].Add(temp)
 
     #odjęcie Pi+1, j pomnożonego przez Xi
-    temp = pyramid[i+1,j]
+    temp = pyramid[i+1][j]
     temp.MultiplyBy(tabX[i])
-    pyramid[i,j].Sub(temp)
+    pyramid[i][j].Sub(temp)
 
     #podzielenie przez (Xj - Xi)
-    pyramid[i,j].DivideBy(tabX[j] - tabX[i])
+    pyramid[i][j].DivideBy(tabX[j] - tabX[i])
   end
 
 #CalculateResult 
