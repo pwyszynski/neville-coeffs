3df1900b39f7904a1eac1b70d28e657c622c21c8
pwyszynski <stuntmankg@gmail.com>
2015-01-06 21:15:34 +0100

    No errors, but not working...

--- lib/VectorOperations.rb
+++ lib/VectorOperations.rb
@@ -1,59 +1,59 @@
 require_relative 'wektor.rb'
 #Coefficients
 
-  def Coefficients(i, j, tabX, tabY, pyramid)
-    #mnozymy przez Xj
-    temp = pyramid[i][j-1]
-    temp.MultiplyBy(tabX[j])
-    pyramid[i][j].SetVector(temp)
+def Coefficients(i, j, tabX, tabY, pyramid)
+  #mnozymy przez Xj
+  temp = pyramid[i][j-1]
+  temp.MultiplyBy(tabX[j])
+  pyramid[i][j].SetVector(temp)
 
-    #odjęcie Pi,j-1 przesuniętego o 1 w prawo
-    temp = pyramid[i][j-1]
-    temp.ShiftRight
-    pyramid[i][j].Sub(temp)
+  #odjęcie Pi,j-1 przesuniętego o 1 w prawo
+  temp = pyramid[i][j-1]
+  temp.ShiftRight
+  pyramid[i][j].Sub(temp)
 
-    #dodanie Pi+1, j przesuniętego o 1 w prawo
-    temp = pyramid[i+1][j]
-    temp.ShiftRight
-    pyramid[i][j].Add(temp)
+  #dodanie Pi+1, j przesuniętego o 1 w prawo
+  temp = pyramid[i+1][j]
+  temp.ShiftRight
+  pyramid[i][j].Add(temp)
 
-    #odjęcie Pi+1, j pomnożonego przez Xi
-    temp = pyramid[i+1][j]
-    temp.MultiplyBy(tabX[i])
-    pyramid[i][j].Sub(temp)
+  #odjęcie Pi+1, j pomnożonego przez Xi
+  temp = pyramid[i+1][j]
+  temp.MultiplyBy(tabX[i])
+  pyramid[i][j].Sub(temp)
 
-    #podzielenie przez (Xj - Xi)
-    pyramid[i][j].DivideBy(tabX[j] - tabX[i])
+  #podzielenie przez (Xj - Xi)
+  pyramid[i][j].DivideBy(tabX[j] - tabX[i])
 
-    return pyramid[i][j]
-  end
+  return pyramid[i][j]
+end
 
 #CalculateResult 
-  def CalculatePolynomialResult(x,y,n)
-    pyramid = Hash.new {|h,k| h[k] = Wektor.new(n)}
+def CalculatePolynomialResult(x,y,n)
+  pyramid = Hash.new {|h,k| h[k] = Wektor.new(n)}
 
-    for i in 0...n do
-        for j in 0...n-i do
-            if (j != i+j) then
-                next
-            end
+  for i in 0...n do
+      for j in 0...n-i do
+          if (j != i+j) then
+              next
+          end
 
-            pyramid[j][j] = Wektor.new(n)
-            pyramid[j][j] = y[j]
-        end
-    end
-
-    for i in 0...n do
-        for j in 0...n-i do
-            if (j == i+j) then
-                next
-            end
-
-            pyramid[j][i+j] = Wektor.new(n)
-            Coefficients(j, i+j, x, y, pyramid)
-        end
-    end
-
-    return pyramid[0][n-1]
-
+          pyramid[j][j] = Wektor.new(n)
+          pyramid[j][j].push(y[j])
+      end
   end
+
+  for i in 0...n do
+      for j in 0...n-i do
+          if (j == i+j) then
+              next
+          end
+
+          pyramid[j][i+j] = Wektor.new(n)
+          Coefficients(j, i+j, x, y, pyramid)
+      end
+  end
+
+  return pyramid[0][n-1]
+
+end
