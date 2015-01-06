6cd9fb013c2b4a6a7cd56deb2cefaf4d9ba6c35c
padamaszek <Ursus707@gmail.com>
2015-01-06 16:08:48 +0100

    VectorHelber more visible syntax

--- lib/VectorHelpers.rb
+++ lib/VectorHelpers.rb
@@ -1,39 +1,39 @@
 require 'wektor.rb'
-
 #helper mający nam zwrócić latexa z wielomianem do wydruku
-def VectorAsPolynomial(vector) 
+def VectorAsPolynomial(vector)
+  result = ""
+  for i in vector.length-1...0 do
 
-	result = ""
+    if(vector[i] == 0)
+      next
+    end
 
-	for i in vector.length-1...0 do
-		if(vector[i] == 0)
-			next
+    if(a != vector.length-1 && wektor[a] > 0)
+      result.concat(" +")
+    elsif(a != vector.length-1 && vector[a] < 0)
+      result.concat(" ")
+    end
+    
+    if(vector[a] == 1)
 
-		if(a != vector.length-1 && wektor[a] > 0)
-			result.concat(" +")
-		elsif(a != vector.length-1 && vector[a] < 0)
-			result.concat(" ")
-
-		if(vector[a] == 1) then
-			if(a>1)
-				result.concat("x^{#{a}}")
-			else
-				result.concat("x")
-		end
-
-		else 
-			if(a > 1)
-				result.concat("{#{vector[a]}}x^{a}")
-			else
-				result.concat("{#{vector[a]}}x")
-	end
+      if(a > 1)
+        result.concat("x^{#{a}}")
+      else
+        result.concat("x")
+      end
+    else
+      if(a > 1)
+        result.concat("{#{vector[a]}}x^{a}")
+      else
+        result.concat("{#{vector[a]}}x")
+      end
+    end
+  end
 
 
-	if(result == "")
-		result.concat(vector[0])
-
-	else
-		result.concat(" +" + vector[0])
-
-	
+  if(result == "")
+    result.concat(vector[0])
+  else
+    result.concat(" +" + vector[0])
+  end
 end
