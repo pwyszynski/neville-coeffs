ac56bab903ecad303a317dd2a95314fe7d97f6fd
Nemeczek <nemeczekes@gmail.com>
2015-01-06 21:40:59 +0100

    Unit test and refactored length to variable

---
+++ spec/unit/VectorOperations_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../spec_helper.rb'
+require_relative '../../lib/VectorOperations.rb'
+require_relative '../../lib/wektor.rb'
+
+
+describe "#Coefficients" do 
+	it "calculates current vector values "
+	pyramid = Hash.new {|h,k| h[k] = Wektor.new(3)}
+	x = Array[1,2,4]
+	y = Array[0,2,12]
+	
+	pyramid[0][0] = Wektor.new(3).SetVector([0,0,0])
+	pyramid[1][1] = Wektor.new(3).SetVector([2,0,0])
+	pyramid[2][2] = Wektor.new(3).SetVector([12,0,0])
+
+	pyramid[0][1] = Wektor.new(3)
+
+	Coefficients(0,1,x,y,pyramid)
+
+	expect(pyramid[0,1]).to eq("-2 2 0")
+
+
+end
