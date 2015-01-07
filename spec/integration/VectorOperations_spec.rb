da383d1d3dcb5616c8fa9fcebe2f0968a72f4c77
Nemeczek <nemeczekes@gmail.com>
2015-01-07 22:23:46 +0100

    Added catalog for integration tests

---
+++ spec/integration/VectorOperations_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../spec_helper.rb'
+require_relative '../../lib/VectorOperations.rb'
+
+
+	describe "#Coefficients" do 
+		it "calculates current vector values " do
+		pyramid = NArray.object(3,3)
+		x = Array[1,2,4]
+		y = Array[0,2,12]
+		
+		pyramid[0,0] = Wektor.new(3)
+		pyramid[0,0].setVector([0,0,0])
+		pyramid[1,1] = Wektor.new(3)
+		pyramid[1,1].setVector([2,0,0])
+		pyramid[2,2] = Wektor.new(3)
+		pyramid[2,2].setVector([12,0,0])
+
+		pyramid[0,1] = Wektor.new(3)
+
+		Coefficients(0,1,x,y,pyramid)
+
+		expect(pyramid[0,1].coords).to eq(Array[-2,2,0])
+	end
+	end
+
+	describe "#CalculatePolynomialResult" do
+		it "it takes two arrays and their lenght and calculates" do
+		expect(CalculatePolynomialResult([1,2,4],[0,2,12],3)).to eq([0,-1,1])
+		end
+	end
+
