9eac6e0c194479da48fc813f6323142c8ae03c4e
pwyszynski <stuntmankg@gmail.com>
2015-01-10 17:41:16 +0100

    Uncommented VectorAsPolynomial

--- lib/wektor.rb
+++ lib/wektor.rb
@@ -61,50 +61,50 @@ class Wektor
 
 
 #Needs reworking for new vector.
-# def VectorAsPolynomial
-#   vector = self	
-#   result = ""
+  def VectorAsPolynomial
+    vector = self	
+    result = ""
 
-#   #COUNTER LOOP FOR TESTING: FIX ME
-#   a = 2
+    #COUNTER LOOP FOR TESTING: FIX ME
+    a = 2
 
-#   while (a > 0) do
+    while (a > 0) do
 
-#     if(vector[a] == 0)
-#       next
-#     end
+      if(vector[a] == 0)
+        next
+      end
 
-#     if(a != vector.length-1 && vector[a] > 0)
-#       result.concat(" +")
-#     elsif(a != vector.length-1 && vector[a] < 0)
-#       result.concat(" ")
-#     end
-    
-#     if(vector[a] == 1)
+      if(a != vector.length-1 && vector[a] > 0)
+        result.concat(" +")
+      elsif(a != vector.length-1 && vector[a] < 0)
+        result.concat(" ")
+      end
+      
+      if(vector[a] == 1)
 
-#       if(a > 1)
-#         result.concat("x^#{a}")
-#       else
-#         result.concat("x")
-#       end
-#     else
-#       if(a > 1)
-#         result.concat("#{vector[a]}x^#{a}")
-#       else
-#         result.concat("#{vector[a]}x")
-#       end
-#     end
+        if(a > 1)
+          result.concat("x^#{a}")
+        else
+          result.concat("x")
+        end
+      else
+        if(a > 1)
+          result.concat("#{vector[a]}x^#{a}")
+        else
+          result.concat("#{vector[a]}x")
+        end
+      end
 
-#     a -= 1
-#   end
+      a -= 1
+    end
 
 
-#   if(result == "")
-#     result.concat(vector[0])
-#   else
-#     result.concat(" +#{vector[0]}")
-#   end
-#   return result
-# end	
+    if(result == "")
+      result.concat(vector[0])
+    else
+      result.concat(" +#{vector[0]}")
+    end
+    return result
+  end	
 
 end
